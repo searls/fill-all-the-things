c90c4759f1b019dd08b0bfd78ee35f94a9925a96
Justin Searls <searls@gmail.com>
2012-03-02 11:37:55 -0500

    updated mutations

--- dist/fill-all-the-things.js
+++ dist/fill-all-the-things.js
@@ -14,7 +14,8 @@ site: https://searls.github.com/fill-all-the-things
   f = window.FillAllTheThings || (window.FillAllTheThings = {});
 
   (function($, _) {
-    f.actions = [
+    var doAll, doFirst, figureOutAValue, mutations, textFills, types;
+    types = [
       {
         test: function(it) {
           return it.is(':checkbox,:radio');
@@ -34,39 +35,99 @@ site: https://searls.github.com/fill-all-the-things
           return true;
         },
         action: function(it, val) {
-          return f.figureOutAValue(it, val);
+          return figureOutAValue(it, val);
+        }
+      }
+    ];
+    textFills = [
+      {
+        test: function(it) {
+          return it.is(':password');
+        },
+        action: function() {
+          return "f1llTh!NG$?";
+        }
+      }, {
+        test: function(it) {
+          return it.is('[type="email"]') || /email/i.test(it.attr('name'));
+        },
+        action: function() {
+          return "fill@llthethings.org";
+        }
+      }, {
+        test: function(it) {
+          return it.is('[type="tel"]');
+        },
+        action: function() {
+          return "123-456-7890";
+        }
+      }, {
+        test: function(it) {
+          return it.is('[type="url"]');
+        },
+        action: function() {
+          return "http://www.w3.org";
+        }
+      }, {
+        test: function(it, val) {
+          return !val;
+        },
+        action: function() {
+          return "Filling a Thing";
+        }
+      }, {
+        test: function() {
+          return true;
+        },
+        action: function(it, val) {
+          return val;
+        }
+      }
+    ];
+    mutations = [
+      {
+        test: function(it) {
+          return it.attr('maxlength');
+        },
+        action: function(it, val) {
+          return val.substring(0, it.attr('maxlength'));
         }
       }
     ];
     f.fill = function() {
       var $inputs;
       return $inputs = $(':input:visible:enabled').val(function(i, val) {
-        var it, newVal;
-        it = $(this);
-        newVal = val;
-        _(f.actions).find(function(o) {
-          if (o.test(it, val)) {
-            newVal = o.action(it, val);
-            return true;
-          }
-        });
-        return newVal;
+        return doFirst(types, [$(this), val]);
       });
     };
-    f.figureOutAValue = function(it, val) {
-      if (it.is(':password')) {
-        return "f1llTh!NG$?";
-      } else if (it.is('[type="email"]') || /email/i.test(it.attr('name'))) {
-        return "fill@llthethings.org";
-      } else if (it.is('[type="tel"]')) {
-        return "123-456-7890";
-      } else if (it.is('[type="url"]')) {
-        return "http://www.w3.org";
-      } else if (!val) {
-        return "Filling a Thing";
-      } else {
-        return val;
-      }
+    figureOutAValue = function(it, val) {
+      val = doFirst(textFills, [it, val]);
+      return doAll(mutations, [it, val]);
+    };
+    doFirst = function(actions, args) {
+      var match;
+      match = void 0;
+      _(actions).find(function(o) {
+        if (o.test.apply(this, args)) {
+          match = o.action.apply(this, args);
+          return true;
+        }
+      });
+      return match;
+    };
+    doAll = function(actions, args) {
+      var thing;
+      thing = {
+        it: args[0],
+        val: args[1]
+      };
+      _(actions).find(function(o) {
+        if (o.test.call(this, thing.it, thing.val)) {
+          thing.val = o.action.call(this, thing.it, thing.val);
+          return true;
+        }
+      });
+      return thing.val;
     };
     return $(function() {
       return f.fill();
